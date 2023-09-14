9f7f09ece2ad02438d6fcae4fb01664ac8b019b6
HidalgoJoni <jonigarciah@gmail.com>
2023-09-14 08:08:13 +0200

    cambio en clase main.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,7 +9,7 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage stage) {
-		stage.setTitle("HolaFX Application");
+		stage.setTitle("Application HolaFX ");
 		stage.show();
 	}
 }
